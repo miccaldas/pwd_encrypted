5abd442b0e46c402498440bdc8b384aa68e2b7e8
mclds <mclds@protonmail.com>
2023-02-27 00:00:57 +0000

    27-02-2023 commit

--- pwd_encrypted/old_pwd_crypt/encrypt_pwds_db.py
+++ pwd_encrypted/old_pwd_crypt/encrypt_pwds_db.py
@@ -8,8 +8,12 @@ from loguru import logger
 from mysql.connector import Error, connect
 
 fmt = "{time} - {name} - {level} - {message}"
-logger.add("../logs/info.log", level="INFO", format=fmt, backtrace=True, diagnose=True)  # noqa: E501
-logger.add("../logs/error.log", level="ERROR", format=fmt, backtrace=True, diagnose=True)  # noqa: E501
+logger.add(
+    "../logs/info.log", level="INFO", format=fmt, backtrace=True, diagnose=True
+)  # noqa: E501
+logger.add(
+    "../logs/error.log", level="ERROR", format=fmt, backtrace=True, diagnose=True
+)  # noqa: E501
 
 subprocess.run(["isort", __file__])
 
@@ -28,7 +32,7 @@ def encrypt_pwds():
     encrypted_pwds = []
 
     try:
-        conn = connect(host="localhost", user="mic", password="xxxx", database="pwd_encrypted")
+        conn = connect(host="localhost", user="mic", password="xxxx", database="pwd")
         cur = conn.cursor()
         query = """ SELECT pwdid, passwd FROM pwd """
         cur.execute(query)
@@ -36,11 +40,9 @@ def encrypt_pwds():
         for i in range(len(records)):
             token = fernet.encrypt(records[i][1].encode())
             encrypted_pwds.append((records[i][0], token))
+        conn.close()
     except Error as e:
         print("Error while connecting to db", e)
-    finally:
-        if conn:
-            conn.close()
 
     print(encrypted_pwds)
     return encrypted_pwds
@@ -59,7 +61,9 @@ def upload_encrypted_passwords():
     crypt_version = encrypt_pwds()
 
     try:
-        conn = connect(host="localhost", user="mic", password="xxxx", database="pwd_encrypted")
+        conn = connect(
+            host="localhost", user="mic", password="xxxx", database="pwd_encrypted"
+        )
         cur = conn.cursor()
         for crypt in crypt_version:
             args = (crypt[1], crypt[0])
--- pwd_encrypted/teste.py
+++ pwd_encrypted/teste.py
@@ -1,12 +1,18 @@
 """Module Docstring"""
+import codecs
 import pickle
 import random
 import string
+from base64 import b64decode, b64encode
 
 import snoop
-from cryptography.fernet import Fernet
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+# from cryptography.fernet import Fernet
 from db_decorator.db_information import db_information
 from english_words import get_english_words_set
+from mysql.connector import Error, connect
 from snoop import pp
 
 
@@ -45,10 +51,147 @@ def teste():
 #  teste()
 
 
-@db_information
-@snoop
-def crypto():
-    master_key = Fernet.generate_key()
-    with open("mk.bin", "wb") as f:
-        pickle.dump(master_key, f)
+# @db_information
+# @snoop
+# def crypto():
+#     # master_key = Fernet.generate_key()
+#     # with open("mk.bin", "wb") as f:
+#     #   pickle.dump(master_key, f)
 
+#     with open("mk.bin", "rb") as f:
+#         mk = pickle.load(f)
+
+#     # print(dec_text)
+
+#     #    try:
+#     #       answers = [d64, 656]
+#     #       conn = connect(host="localhost", user="mic", password="xxxx", database="pwd")
+#     #      cur = conn.cursor()
+#     #      query = "UPDATE pwd SET test = %s WHERE pwdid = %s"
+#     #     cur.execute(query, answers)
+#     #    conn.commit()
+#     #  except Error as e:
+#     #     err_msg = "Error while connecting to db", e
+#     #    print("Error while connecting to db", e)
+#     #   if err_msg:
+#     #      return query, err_msg
+#     #  finally:
+#     #     if conn:
+#     #        conn.close()
+
+#     with open("id_pwd.bin", "rb") as f:
+#         records = pickle.load(f)
+# #    cipher_suite = Fernet(mk)
+# #    cipher_text = cipher_suite.encrypt('3PU5wne!gIGYAF"=dcqMLa~'.encode())
+#     # d64 = b64encode(cipher_text)
+
+#     # print(f"d64 is: {d64}")
+# #    print(cipher_text)
+
+#     try:
+#         conn = connect(host="localhost", user="mic", password="xxxx", database="pwd")
+#         cur = conn.cursor()
+#  #       answers = [f"{cipher_text}", "657"]
+#         query = "UPDATE pwd SET pwd = %s WHERE pwdid = %s"
+#         cur.execute(query, answers)
+#         conn.commit()
+#     except Error as e:
+#         err_msg = "Error while connecting to db", e
+#         print("Error while connecting to db", e)
+#         if err_msg:
+#             return query, err_msg
+#     finally:
+#         if conn:
+#             conn.close()
+
+#     try:
+#         answers = [657]
+#         conn = connect(host="localhost", user="mic", password="xxxx", database="pwd")
+#         cur = conn.cursor()
+#         query = "SELECT pwd FROM pwd WHERE pwdid = %s"
+#         cur.execute(query, answers)
+#         record = cur.fetchone()
+#     except Error as e:
+#         err_msg = "Error while connecting to db", e
+#         print("Error while connecting to db", e)
+#         if err_msg:
+#             return query, err_msg
+#     finally:
+#         if conn:
+#             conn.close()
+
+#     print(record)
+#     # decs = b64decode(record[0])
+#     print(record[0])
+# #    uncipher = cipher_suite.decrypt(record[0])
+# #    pwd_str = bytes(uncipher).decode("utf-8")
+# #    print(pwd_str)
+#  #   print(type(pwd_str))
+
+
+@snoop
+def crypto_dome():
+    data = b"|NjRJl[3%_)kZV=!<}hHzQw"
+    #key = get_random_bytes(16)
+    #with open("aes_key.bin", "wb") as f:
+   #     pickle.dump(key, f)
+
+    with open("aes_key.bin", "rb") as f:
+        aes_key = pickle.load(f)
+    print(aes_key)
+
+    cipher = AES.new(aes_key, AES.MODE_EAX)
+    ciphertext, tag = cipher.encrypt_and_digest(data)
+    nonce = cipher.nonce
+
+    try:
+        conn = connect(host="localhost", user="mic", password="xxxx", database="pwd")
+        cur = conn.cursor()
+        answers = [f"{ciphertext}", f"{tag}", "657"]
+        query = "UPDATE pwd SET pwd = %s,  tag = %s WHERE pwdid = %s"
+        cur.execute(query, answers)
+        conn.commit()
+    except Error as e:
+        err_msg = "Error while connecting to db", e
+        print("Error while connecting to db", e)
+        if err_msg:
+            return query, err_msg
+    finally:
+        if conn:
+            conn.close()
+
+    try:
+        answers = [657]
+        conn = connect(host="localhost", user="mic", password="xxxx", database="pwd")
+        cur = conn.cursor()
+        query = "SELECT tag, pwd FROM pwd WHERE pwdid = %s"
+        cur.execute(query, answers)
+        records = cur.fetchall()
+    except Error as e:
+        err_msg = "Error while connecting to db", e
+        print("Error while connecting to db", e)
+        if err_msg:
+            return query, err_msg
+    finally:
+        if conn:
+            conn.close()
+
+    # file_out = open("encrypted.bin", "wb")
+    # [file_out.write(x) for x in (ciphertext)]
+    # file_out.close()
+
+    # with open("encrypted.bin", "rb") as f:
+    #     ciphertext = [f.read(x) for x in (16, 16, -1)]
+
+    print(records)
+    print(type(records[0][0]))
+    print(type(records[0][1]))
+    ciphertext = records[0][1]
+    cipher = AES.new(key, AES.MODE_EAX, nonce)
+    data = cipher.decrypt_and_verify(ciphertext, tag)
+    datastr = data.decode()
+    print(datastr)
+
+
+if __name__ == "__main__":
+    crypto_dome()
